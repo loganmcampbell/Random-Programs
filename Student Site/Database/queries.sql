7ea10439bea0f06a799e6ec24da9b50e8476e9e4
Logan Malachi Campbell <lc002@email.uark.edu>
2019-09-22 20:22:52 -0500

    Add files via upload

---
+++ Student Site/Database/queries.sql
@@ -0,0 +1,16 @@
+
+CREATE DATABASE students_db;
+USE student_db;
+
+show databases;
+use students_db;
+show tables;
+describe tables;
+
+CREATE TABLE Students
+(
+	FirstName varchar(255),
+	LastName varchar(255),
+	SID varchar(255),
+	Major varchar(255)
+);
